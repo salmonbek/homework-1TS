cd711abb4002b28c2d73fcc721d3dc16696d9d17
“salmonbek” <salmonlearner34@gmail.com>
2024-04-25 19:26:06 +0500

    calculate

--- main.js
+++ main.js
@@ -1,2 +1,38 @@
-var numberR = 22;
-console.log(number);
+// calculator start
+
+function add() {
+  var num1 = parseFloat(document.getElementById("num1").value);
+  var num2 = parseFloat(document.getElementById("num2").value);
+  var result = num1 + num2;
+  displayResult(result);
+}
+function subtract() {
+  var num1 = parseFloat(document.getElementById("num1").value);
+  var num2 = parseFloat(document.getElementById("num2").value);
+  var result = num1 - num2;
+  displayResult(result);
+}
+function multiply() {
+  var num1 = parseFloat(document.getElementById("num1").value);
+  var num2 = parseFloat(document.getElementById("num2").value);
+  var result = num1 * num2;
+  displayResult(result);
+}
+function divide() {
+  var num1 = parseFloat(document.getElementById("num1").value);
+  var num2 = parseFloat(document.getElementById("num2").value);
+  if (num2 === 0) {
+    displayResult("Cannot divide by zero");
+  } else {
+    var result = num1 / num2;
+    displayResult(result);
+  }
+}
+function displayResult(value) {
+  var resultElement = document.getElementById("result");
+  if (resultElement) {
+    resultElement.textContent = value.toString();
+  }
+}
+
+// calculator end
--- main.ts
+++ main.ts
@@ -1,2 +1,59 @@
-let numberR: number = 22;
-console.log(numberR);
+// CALCULATOR --THERE ARE (ADD, SUBTRACT, MULTIPLY,DIVIDE,)
+
+function add() {
+  const num1 = parseFloat(
+    (<HTMLInputElement>document.getElementById("num1")).value
+  );
+  const num2 = parseFloat(
+    (<HTMLInputElement>document.getElementById("num2")).value
+  );
+  const result = num1 + num2;
+  displayResult(result);
+}
+
+function subtract() {
+  const num1 = parseFloat(
+    (<HTMLInputElement>document.getElementById("num1")).value
+  );
+  const num2 = parseFloat(
+    (<HTMLInputElement>document.getElementById("num2")).value
+  );
+  const result = num1 - num2;
+  displayResult(result);
+}
+
+function multiply() {
+  const num1 = parseFloat(
+    (<HTMLInputElement>document.getElementById("num1")).value
+  );
+  const num2 = parseFloat(
+    (<HTMLInputElement>document.getElementById("num2")).value
+  );
+  const result = num1 * num2;
+  displayResult(result);
+}
+
+function divide() {
+  const num1 = parseFloat(
+    (<HTMLInputElement>document.getElementById("num1")).value
+  );
+  const num2 = parseFloat(
+    (<HTMLInputElement>document.getElementById("num2")).value
+  );
+
+  if (num2 === 0) {
+    displayResult("Cannot divide by zero");
+  } else {
+    const result = num1 / num2;
+    displayResult(result);
+  }
+}
+
+function displayResult(value: string | number) {
+  const resultElement = document.getElementById("result");
+  if (resultElement) {
+    resultElement.textContent = value.toString();
+  }
+}
+
+// CALCULATOR END HERE
